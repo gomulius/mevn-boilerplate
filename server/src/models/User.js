eaf9c04e5d8a75a2af065e0a9318f75462995f32
GeorgeEYokoyama <george_y@outlook.com>
2018-04-01 21:53:20 -0700

    Create User model

---
+++ server/src/models/User.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+const bcrypt = require('bcrypt-nodejs')
+
+const UserSchema = new Schema({
+  username: { type: String, lowercase: true, required: true, unique: true },
+  password: { type: String, required: true },
+  email: { type: String, lowercase: true, required: true, unique: true }
+})
+
+UserSchema.pre('save', function (next) {
+  const user = this
+  bcrypt.hash(user.password, null, null, function (err, hash) {
+    if (err) return next(err)
+    user.password = hash
+    next()
+  })
+})
+
+module.exports = mongoose.model('User', UserSchema)
