28e9b18274727035df90615a8b7b495cdb55c9c0
GeorgeEYokoyama <george_y@outlook.com>
2018-04-02 19:36:21 -0700

    AuthController with error codes

--- server/src/controllers/AuthController.js
+++ server/src/controllers/AuthController.js
@@ -8,11 +8,11 @@ module.exports = {
     user.password = req.body.password
     user.email = req.body.email
     if (req.body.username == null || req.body.username === '' || req.body.password == null || req.body.password === '' || req.body.email == null || req.body.email === '') {
-      res.json({ success: false, message: 'Ensure username, password, and email were provided.' })
+      res.status(400).json({ success: false, message: 'Ensure username, password, and email were provided.' })
     } else {
       user.save((err) => {
         if (err) {
-          res.json({ success: false, message: 'Username or email already exists!' })
+          res.status(409).json({ success: false, message: 'Username or email already exists!' })
         } else {
           res.json({ success: true, message: 'User created!' })
         }
