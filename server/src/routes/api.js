df9eec1383a85ed18d8bd74fe7215f3f836585a7
GeorgeEYokoyama <george_y@outlook.com>
2018-04-02 17:07:54 -0700

    Separate logic from endpoints

--- server/src/routes/api.js
+++ server/src/routes/api.js
@@ -1,25 +1,10 @@
-const User = require('../models/user')
+// const User = require('../models/user')
+const AuthController = require('../controllers/AuthController')
 
-module.exports = function (router) {
+module.exports = (router) => {
   /* REGISTRATION */
-  // http://localhost:8081/api/users
-  router.post('/register', (req, res) => {
-    const user = new User()
-    user.username = req.body.username
-    user.password = req.body.password
-    user.email = req.body.email
-    if (req.body.username == null || req.body.username === '' || req.body.password == null || req.body.password === '' || req.body.email == null || req.body.email === '') {
-      res.json({ success: false, message: 'Ensure username, password, and email were provided.' })
-    } else {
-      user.save((err) => {
-        if (err) {
-          res.json({ success: false, message: 'Username or email already exists!' })
-        } else {
-          res.json({ success: true, message: 'User created!' })
-        }
-      })
-    }
-  })
+  // http://localhost:8081/api/register
+  router.post('/register', AuthController.register )
 
   return router
 }
