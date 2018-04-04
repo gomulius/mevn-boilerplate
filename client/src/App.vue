4490facd8a5fed3a7fa9e1f4b74e21e083c058a0
GeorgeEYokoyama <george_y@outlook.com>
2018-04-03 17:27:02 -0700

    Add Vuetify

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,7 +1,12 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <v-app>
+      <v-content>
+        <v-container>
+          <router-view/>
+        </v-container>
+      </v-content>
+    </v-app>
   </div>
 </template>
 
@@ -16,8 +21,6 @@ export default {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
