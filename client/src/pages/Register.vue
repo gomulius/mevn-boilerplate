ca3a59042cc2408f42318dbee31d222798f9bf49
GeorgeEYokoyama <george_y@outlook.com>
2018-04-03 19:27:17 -0700

    Create Registration form

--- client/src/pages/Register.vue
+++ client/src/pages/Register.vue
@@ -1,14 +1,48 @@
 <template lang="html">
-  <div>
-    <h1>Register</h1>
+  <v-container fill-height>
+    <v-layout align-center justify-center>
 
-    <form @submit="register">
-      <input v-model='email' type="email" name="email" placeholder="Enter email">
-      <input v-model='password' type="password" name="password" placeholder="Enter Password">
-      <input v-model='username' type="username" name="username" placeholder="Enter username">
-      <input type="submit" name="register" value="">
-    </form>
-  </div>
+      <v-flex xs12 sm8 md6>
+        <v-card class="elevation-6">
+          <v-toolbar dark class="teal darken-2">
+            <v-toolbar-title color="white">Registration Form</v-toolbar-title>
+          </v-toolbar>
+
+          <v-container>
+            <v-form v-model="valid" ref="form" lazy-validation @submit="register">
+              <v-text-field
+              v-model="username"
+              type="text"
+              label="Enter username:"
+              :rules="[rules.required]"
+              ></v-text-field>
+              <v-text-field
+              v-model="password"
+              type="password"
+              label="Enter password:"
+              :rules="[rules.required]"
+              ></v-text-field>
+              <v-text-field
+              v-model="email"
+              type="email"
+              label="Enter email address:"
+              :rules="[rules.required]"
+              ></v-text-field>
+              <v-text-field
+              v-model="emailCheck"
+              type="email"
+              label="Enter email address again"
+              :rules="[rules.required, rules.email]"
+              ></v-text-field>
+              <v-btn type="submit" :disabled="!valid">Register</v-btn>
+            </v-form>
+          </v-container>
+        </v-card>
+
+      </v-flex>
+
+    </v-layout>
+  </v-container>
 </template>
 
 <script>
@@ -17,23 +51,31 @@ export default {
   name: 'Register',
   data () {
     return {
+      valid: true,
       email: '',
+      emailCheck: '',
       password: '',
       username: '',
-      error: null
+      error: null,
+      rules: {
+        required: v => !!v || 'Required.',
+        email: v => v === this.email || 'Please check your email address.'
+      }
     }
   },
   methods: {
     async register (e) {
       e.preventDefault()
-      try {
-        await AuthService.register({
-          username: this.username,
-          password: this.password,
-          email: this.email
-        })
-      } catch (e) {
-        this.error = e.response.data
+      if (this.$refs.form.validate()) {
+        try {
+          await AuthService.register({
+            username: this.username,
+            password: this.password,
+            email: this.email
+          })
+        } catch (e) {
+          this.error = e.response.data
+        }
       }
     }
   }
