baa62b0eeccc47b9135beb952ec92f16491c221a
GeorgeEYokoyama <george_y@outlook.com>
2018-04-05 17:20:41 -0700

    Create and Use Panel component

---
+++ client/src/components/Panel.vue
@@ -0,0 +1,23 @@
+<template lang="html">
+  <v-card class="elevation-6">
+    <v-toolbar dense dark class="teal darken-2">
+      <v-toolbar-title>{{ title }}</v-toolbar-title>
+    </v-toolbar>
+
+    <v-container>
+      <slot>No slot content defined.</slot>
+    </v-container>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: 'Panel',
+  props: [
+    'title'
+  ]
+}
+</script>
+
+<style lang="css">
+</style>
--- client/src/pages/Register.vue
+++ client/src/pages/Register.vue
@@ -1,54 +1,47 @@
 <template lang="html">
   <v-layout align-center justify-center>
-
     <v-flex xs12 sm8 md6>
-      <v-card class="elevation-6">
-        <v-toolbar dense dark class="teal darken-2">
-          <v-toolbar-title>Registration Form</v-toolbar-title>
-        </v-toolbar>
 
-        <v-container>
+      <panel title="Register Form">
+        <v-alert outline transition="scale-transition" color="error" icon="warning" :value="!!error">{{ error }}</v-alert>
+        <v-alert outline transition="scale-transition" color="success" icon="check_circle" :value="!!message">{{ message }}</v-alert>
 
-          <v-alert outline transition="scale-transition" color="error" icon="warning" :value="!!error">{{ error }}</v-alert>
-          <v-alert outline transition="scale-transition" color="success" icon="check_circle" :value="!!message">{{ message }}</v-alert>
-
-          <v-form v-model="valid" ref="form" lazy-validation @submit="register">
-            <v-text-field
-            v-model="username"
-            type="text"
-            label="Enter username:"
-            :rules="[rules.required]"
-            ></v-text-field>
-            <v-text-field
-            v-model="password"
-            type="password"
-            label="Enter password:"
-            :rules="[rules.required]"
-            ></v-text-field>
-            <v-text-field
-            v-model="email"
-            type="email"
-            label="Enter email address:"
-            :rules="[rules.required]"
-            ></v-text-field>
-            <v-text-field
-            v-model="emailCheck"
-            type="email"
-            label="Enter email address again"
-            :rules="[rules.required, rules.email]"
-            ></v-text-field>
-            <v-btn type="submit" :disabled="!valid">Register</v-btn>
-          </v-form>
-        </v-container>
-      </v-card>
+        <v-form v-model="valid" ref="form" lazy-validation @submit="register">
+          <v-text-field
+          v-model="username"
+          type="text"
+          label="Enter username:"
+          :rules="[rules.required]"
+          ></v-text-field>
+          <v-text-field
+          v-model="password"
+          type="password"
+          label="Enter password:"
+          :rules="[rules.required]"
+          ></v-text-field>
+          <v-text-field
+          v-model="email"
+          type="email"
+          label="Enter email address:"
+          :rules="[rules.required]"
+          ></v-text-field>
+          <v-text-field
+          v-model="emailCheck"
+          type="email"
+          label="Enter email address again"
+          :rules="[rules.required, rules.email]"
+          ></v-text-field>
+          <v-btn type="submit" :disabled="!valid">Register</v-btn>
+        </v-form>
+      </panel>
 
     </v-flex>
-
   </v-layout>
 </template>
 
 <script>
 import AuthService from '@/services/AuthService'
+import Panel from '@/components/Panel'
 export default {
   name: 'Register',
   data () {
@@ -84,6 +77,9 @@ export default {
         }
       }
     }
+  },
+  components: {
+    Panel
   }
 }
 </script>
