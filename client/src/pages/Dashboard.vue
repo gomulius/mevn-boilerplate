77ff317bbcf7aa5403f69c012e7b610dea3bae50
GeorgeEYokoyama <george_y@outlook.com>
2018-04-10 19:37:27 -0700

    Fix style

--- client/src/pages/Dashboard.vue
+++ client/src/pages/Dashboard.vue
@@ -1,27 +1,29 @@
 <template lang="html">
-  <v-layout>
-    <panel title="Posts">
-        <v-btn
-          flat
-          absolute
-          right
-          slot="toolbar-item"
-          :to="{ name: 'NewPost' }"
-          >+ Add</v-btn>
-        <table v-if="items != ''">
-          <tr>
-            <th>Date</th>
-            <th>title</th>
-            <th>body</th>
-          </tr>
-          <tr v-for="item in items" :key="item._id">
-            <td>{{ item.date }}</td>
-            <td>{{ item.title }}</td>
-            <td>{{ item.body }}</td>
-            <v-btn :to="{ name: 'ViewPost', params: { id: item._id } }">View</v-btn>
-          </tr>
-        </table>
-    </panel>
+  <v-layout justify-center>
+    <v-flex xs12 sm8 md6>
+      <panel title="Posts">
+          <v-btn
+            flat
+            absolute
+            right
+            slot="toolbar-item"
+            :to="{ name: 'NewPost' }"
+            >+ Add</v-btn>
+          <table v-if="items != ''">
+            <tr>
+              <th>Date</th>
+              <th>title</th>
+              <th>body</th>
+            </tr>
+            <tr v-for="item in items" :key="item._id">
+              <td>{{ item.date }}</td>
+              <td>{{ item.title }}</td>
+              <td>{{ item.body }}</td>
+              <v-btn :to="{ name: 'ViewPost', params: { id: item._id } }">View</v-btn>
+            </tr>
+          </table>
+      </panel>
+    </v-flex>
   </v-layout>
 </template>
 
