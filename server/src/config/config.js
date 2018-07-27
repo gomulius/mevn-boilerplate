c7ff2f6f72af4ba7f85c6be246a660953e78093e
GeorgeEYokoyama <george_y@outlook.com>
2018-07-27 13:03:55 -0700

    Mongoose use new url string parser option

--- server/src/config/config.js
+++ server/src/config/config.js
@@ -1,7 +1,8 @@
 module.exports = {
   port: process.env.PORT || 8081,
   mongoose: {
-    uri: 'mongodb://localhost:27017/mevn-boilerplate'
+    uri: 'mongodb://localhost:27017/mevn-boilerplate',
+    options: { useNewUrlParser: true }
   },
   jwt: {
     secret: process.env.JWT_SECRET || 'secret'
